7ffc918d872fa8ca35e6786719f31692d74efbc5
JavierVivar <javiertxu71@hotmail.com>
2021-05-12 19:35:12 +0200

    Version 2 Proyectp cuadro de mando con beans dao repository y commit convocatoriasspring_web_cuadro_mando_V2.rar

--- cuadro_mando.sql
+++ cuadro_mando.sql
@@ -1,24 +1,29 @@
 create database cuadro_mando;
 use cuadro_mando;
 
+drop table convocatorias;
 create table convocatorias
-(idConvocatoria varchar(45) not null primary key,
+(idConvocatoria int not null auto_increment primary key, 
+codigoConvocatoria varchar(45),
 denominacion varchar(50) not null,
 ambito varchar(25) not null,
 anualidad int not null
 );
 
+drop table expedientes; 
 create table expedientes
-(codigoExpediente varchar(25) not null primary key,
+(idExpediente int not null auto_increment primary key,
+codigoExpediente varchar(25) not null,
 sector varchar(45) not null,
-idConvocatoria varchar(45) not null,
-idEntidad varchar(12) not null,
+idConvocatoria int not null,
+idEntidad int not null,
 foreign key(idConvocatoria) references convocatorias(idConvocatoria),
 foreign key(idEntidad) references entidades(idEntidad)
 );
 
+drop table entidades; 
 create table entidades
-(idEntidad varchar(12) not null primary key,
+(idEntidad int not null auto_increment primary key,
 cif_Entidad varchar(9) not null,
 nombreEntidad varchar(85) not null,
 direccionEntidad varchar(150),
@@ -26,25 +31,29 @@ CP_Entidad int,
 ciudadEntidad varchar(25),
 provinciaEntidad varchar(25)
 );
+DROP TABLE accionesFormativas;
 
 create table accionesFormativas
-(codigoAAFF varchar(20) not null primary key,
+(idAAFF int not null auto_increment primary key,
+codigoAAFF varchar(20) not null,
 denominacionAAFF varchar(90) not null
 );
 
+drop table usuarios; 
 create table usuarios
 (idUsuario int not null auto_increment primary key,
 nombreUsuario varchar(15) not null,
-password varchar(10) not null
+pass varchar(10) not null
 );
 
+ DROP TABLE grupos; 
 create table grupos
 (idGrupo int not null auto_increment primary key,
-codigoExpediente varchar(25) not null,
+idExpediente int not null,
 numeroAAFF int not null,
 numeroGrupo int not null,
-idEntidad varchar(12),
-codigoAAFF varchar(20),
+idEntidad int,
+idAAFF int not null,
 modalidad varchar(10) not null, 
 horasTotales int,
 horasP int,
@@ -55,8 +64,13 @@ fechaInicio date,
 fechafin date, 
 alumnosiniciados int, 
 alumnosfinalizados int, 
-foreign key(codigoExpediente) references expedientes(codigoExpediente),
+foreign key(idExpediente) references expedientes(idExpediente),
 foreign key(idEntidad) references entidades(idEntidad),
-foreign key(codigoAAFF) references accionesFormativas(codigoAAFF)
+foreign key(idAAFF) references accionesFormativas(idAAFF)
 );
 
+insert into familias values(1,'bebidas'),(2,'ropa'),(3,'zapatos');
+INSERT INTO `convocatorias` (`idConvocatoria`,`codigoConvocatoria`,`denominacion`,`ambito`,`anualidad`) VALUES (1,'Ocupados 2018','Estatal Ocupados 2018','Nacional',2018);
+INSERT INTO `convocatorias` (`idConvocatoria`,`codigoConvocatoria`,`denominacion`,`ambito`,`anualidad`) VALUES (2,'Ocupados 2028','Estatal Ocupados 2028','Nacional',2020);
+INSERT INTO `convocatorias` (`idConvocatoria`,`codigoConvocatoria`,`denominacion`,`ambito`,`anualidad`) VALUES (3,'TICS 2018','Estatal TICS 2018','Nacional',2018);
+INSERT INTO `convocatorias` (`idConvocatoria`,`codigoConvocatoria`,`denominacion`,`ambito`,`anualidad`) VALUES (4,'TICS 2020','Estatal TICS 2020','Nacional',2020);
